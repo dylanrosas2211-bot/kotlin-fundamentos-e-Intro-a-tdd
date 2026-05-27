440c925b610b5f164cd27dd70667ac2daeb2df62
Bautista <bautistamoyano1606@gmail.com>
2026-05-27 11:44:02 -0300

    termine el ejercicio 4

--- src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio4.kt
+++ src/main/kotlin/edu/etec/ds/fundamentos/Ejercicio4.kt
@@ -1,21 +1,63 @@
 package edu.etec.ds.fundamentos
 
 fun evaluarPositivo(numero: Int): String {
-    TODO("Retornar \"Positivo\" si numero > 0, sino \"No positivo\"")
+    val resultado = if (numero > 0) "Positivo" else "No positivo"
+    return resultado
 }
 
 fun clasificarNumero(numero: Int): String {
-    TODO("Clasificar: Mayor a 10, Menor a 10, o Igual a 10")
+    if (numero > 10){
+        return "Mayor a 10"
+    }
+    if (numero < 10){
+        return "Menor a 10"
+    }
+    return "Igual a 10"
 }
 
 fun evaluarEdad(edad: Int): String {
-    TODO("Menor de edad: < 18, Adulto: 18-65, Adulto mayor: > 65")
+    if (edad < 18){
+        return "Menor de edad"
+    }
+    if (edad >= 18 && edad <= 65){
+        return "Adulto"
+    }
+        return "Adulto mayor"
 }
 
+
 fun obtenerDescuento(monto: Double): Double {
-    TODO("Retornar descuento: 10% si monto > 100, 20% si > 150, 30% si > 200, sino 0%")
+   if (monto >= 100 && monto < 150) {
+       return 10.0
+   }
+    if (monto >= 150 && monto < 200) {
+        return 20.0
+    }
+    if (monto >= 200) {
+        return 30.0
+    }
+    return 0.0
 }
 
 fun calcular(operador: Char, a: Int, b: Int): Double {
+    if (operador == '+' ){
+        return (a + b) .toDouble()
+    }
+    if (operador == '-' ){
+        return (a - b) .toDouble()
+    }
+    if (operador == '*' ){
+        return (a * b) .toDouble()
+    }
+    if (operador == '/' ){
+        if (b == 0) {
+            return 0.0
+        }
+        return (a / b) .toDouble()
+    }
+
+    return 0.0
+
+
     TODO("Realizar operacion: + - * /. Retornar 0.0 si division por cero u operador invalido")
 }
